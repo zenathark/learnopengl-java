e2d977431ac7828c6797aef8dbdc199d2ed57b99
Juan Carlos Galan Hernandez <jcgalanhlanh@gmail.com>
2019-09-13 07:49:24 -0500

    Try fixing thread on mac, not completed

--- src/main/java/learnopengl/chap3/HelloWindow.java
+++ src/main/java/learnopengl/chap3/HelloWindow.java
@@ -13,8 +13,16 @@ import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryStack.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
-public class HelloWindow {
-    public static long init() {
+public class HelloWindow implements Runnable {
+    long window = 0;
+    private Thread thread;
+
+    public void start() {
+        thread = new Thread(this, "Game");
+        thread.start();
+    }
+
+    public void init() {
         GLFWErrorCallback.createPrint(System.err).set();
         if (!glfwInit())
             throw new IllegalStateException("Unable to initialize GLFW");
@@ -22,7 +30,7 @@ public class HelloWindow {
         glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-        var window = glfwCreateWindow(300, 300, "Hello, World!", NULL, NULL);
+        window = glfwCreateWindow(300, 300, "Hello, World!", NULL, NULL);
         glfwSetKeyCallback(window, (w, key, scancode, action, mods) -> {
             if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
                 glfwSetWindowShouldClose(w, true);
@@ -44,23 +52,19 @@ public class HelloWindow {
         glfwMakeContextCurrent(window);
         glfwSwapInterval(1);
         glfwShowWindow(window);
-        return window;
     }
 
-    public static void loop(long window) {
+    @Override
+    public void run() {
         GL.createCapabilities();
+        System.out.printf("Hello LWJGL %s", Version.getVersion());
+        init();
         glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
         while (!glfwWindowShouldClose(window)) {
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
             glfwSwapBuffers(window);
             glfwPollEvents();
         }
-    }
-
-    public static void run() {
-        System.out.printf("Hello LWJGL %s", Version.getVersion());
-        var w = init();
-        loop(w);
         glfwFreeCallbacks(w);
         glfwDestroyWindow(w);
         glfwTerminate();
